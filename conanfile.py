15d13978f8856eeff2434fca4f0a03c515d1e6ff
tharada <harada.tomoyuki03@gmail.com>
2017-02-14 15:48:54 +0900

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile, tools
+import os
+
+
+class CatchConan(ConanFile):
+    name = "Catch"
+    version = "1.7.2"
+    description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD C++ Automated Test Cases in Headers"
+    license = "Boost"
+    url = "https://github.com/gasuketsu/conan-catch"
+    settings = None
+
+    def source(self):
+        tools.download("https://github.com/philsquared/Catch/releases/download/v%s/catch.hpp" % self.version, "catch.hpp")
+
+    def build(self):
+        pass
+
+    def package(self):
+        self.copy("catch.hpp", dst="include", src=".")
+
+    def package_info(self):
+        self.cpp_info.libdirs = []
